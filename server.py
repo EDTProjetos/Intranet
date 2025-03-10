834b8607016f597c88583410c8c61750057974a5
EDTProjetos <raphael.barbosa@energiadetodos.com.br>
2025-03-10 17:25:23 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,17 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import threading
 import subprocess
 import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+# Configuração do Cloudinary
+cloudinary.config(
+    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),  # Substitua com suas variáveis de ambiente
+    api_key=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET')
+)
 
 app = Flask(__name__)
 script_finalizado = False
@@ -51,6 +61,25 @@ def executar_script_api():
 @app.route("/status", methods=["GET"])
 def verificar_status():
     return jsonify({"script_finalizado": script_finalizado})
+
+# Rota para o upload da imagem para o Cloudinary
+@app.route("/upload-image", methods=["POST"])
+def upload_image():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+
+    try:
+        # Fazendo o upload da imagem para o Cloudinary
+        upload_result = cloudinary.uploader.upload(file)
+        image_url = upload_result['secure_url']  # URL segura da imagem
+
+        # Retornando a URL da imagem
+        return jsonify({"image_url": image_url})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
